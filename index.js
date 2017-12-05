cb9fe3441fb1db44ba66fbb64f7e2fb49b11fda7
Monika Williams <monika.m.williams@gmail.com>
2017-12-05 09:31:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+function updateDriverWithKeyAndValue() {
+  const driver = {};
+}
+function destructivelyUpdateDriverWithKeyAndValue() {
+  
+}
+function deleteFromDriverByKey() {
+  
+}
+function destructivelyDeleteFromDriverByKey() {
+  delete driver.name;
+}
