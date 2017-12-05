65c3a8c7d4c4bafba2f56af33db3c71bc54f041e
Monika Williams <monika.m.williams@gmail.com>
2017-12-05 09:41:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // Write your solution in this file!
 const driver = {};
 function updateDriverWithKeyAndValue(driver, key , value) {
- driver[key] = value;
+  const newDriver = {...driver};
+  newDriver[address] = '11 Broadway';
+  return newDriver;
 }
 function destructivelyUpdateDriverWithKeyAndValue() {
 
 }
-function deleteFromDriverByKey() {
+function deleteFromDriverByKey(driver, key) {
 
 }
 function destructivelyDeleteFromDriverByKey(driver, name) {
